587196e85bd7e9fc8a8bcdb4a8fe47802da4e4da
Simon Norberg <simon.norberg@gmail.com>
2021-05-15 15:51:19 +0200

    Update Oboe

--- sample/src/main/java/net/simno/kortholt/SampleService.kt
+++ sample/src/main/java/net/simno/kortholt/SampleService.kt
@@ -1,16 +1,16 @@
 package net.simno.kortholt
 
 import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.content.getSystemService
+import androidx.core.app.NotificationManagerCompat
 
 class SampleService : Service() {
 
@@ -20,6 +20,7 @@ class SampleService : Service() {
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        createNotificationChannel()
         startForeground(NOTIFICATION_ID, createNotification())
         return START_STICKY
     }
@@ -32,13 +33,16 @@ class SampleService : Service() {
         super.onDestroy()
     }
 
-    private fun createNotification(): Notification {
-        val manager = getSystemService<NotificationManager>()
+    private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = manager?.getNotificationChannel(CHANNEL_ID)
-                ?: NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_LOW)
-            manager?.createNotificationChannel(channel)
+            val channel = NotificationChannelCompat.Builder(CHANNEL_ID, IMPORTANCE_LOW)
+                .setName("Playback")
+                .build()
+            NotificationManagerCompat.from(applicationContext).createNotificationChannel(channel)
         }
+    }
+
+    private fun createNotification(): Notification {
         val intent = Intent(this, SampleActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
         return NotificationCompat.Builder(this, CHANNEL_ID)
